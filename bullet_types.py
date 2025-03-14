31a79bfe00ef02b75299e537866088905cbfb139
Lim95 <73658212+Lim98SE@users.noreply.github.com>
2025-03-14 14:47:14 -0600

    fix bullet

--- bullet_types.py
+++ bullet_types.py
@@ -6,12 +6,8 @@ class Bullet:
     def __init__(self, target, position):
         self.position = pygame.Vector2(position)
         self.target = pygame.Vector2(target)
-        self.angle = self.position.angle_to(target)
-        radians = math.atan2(target.x-position.y, target.x-position.x)
-        
-        angle = -math.degrees(radians)
-        self.direction = pygame.Vector2(1, 0).rotate(angle)
-        print(self.angle, self.direction, self.target)
+        self.direction = pygame.Vector2(self.target.x - self.position.x, self.target.y - self.position.y) # stolen from godot: https://github.com/godotengine/godot/blob/87f897ae0ad5a6770c6ee267e2f3458c89a06df7/core/math/vector2.h#L302
+        self.direction.normalize_ip()
     
     def tick(self):
         self.position += self.direction * 10
