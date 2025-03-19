d76a14ab784cb790b8bcdfeafbdcbd51d3d5fd82
Lim95 <73658212+Lim98SE@users.noreply.github.com>
2025-03-19 15:00:51 -0600

    collision !

--- Enemy.py
+++ Enemy.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 from utils import dir_to
+from bullet_types import Bullet
+
+from main import shoot
 
 # setup_extra_vars() -> use to uh, setup extra self-scope variables
 # tick(self, target) -> ran every game tick, target should be whatever position it's targeting
@@ -10,19 +13,39 @@ class Enemy:
     def setup_extra_vars(self): # define this per class
         return
 
-    def __init__(self, position, color, size):
+    def __init__(self, position, color, size, hp):
         self.position = pygame.Vector2(position)
         self.size = pygame.Vector2(size)
         self.color = color
+        self.hitbox = pygame.Rect(self.position, self.size)
+        self.hp = hp
+        self.isAlive = True
 
         self.setup_extra_vars()
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.position, self.size))
 
-    def tick(self, target):
+    def tick(self, target, ticks = 0):
         raise NotImplementedError("needs to be overridden by subclass")
+    
+    def collision(self, bulletPos):
+        return self.hitbox.collidepoint(bulletPos)
+    
+    def collide_all(self, bullets):
+        for i in bullets:
+            if self.hitbox.collidepoint(i.position):
+                bullets.remove(i)
+                return True
+        
+        return False
 
+    def LifeCheck(self):
+        if self.hp <= 0:
+            self.isAlive = False
+        else:
+            self.isAlive = True
+                                                                                                                                        #ik and ive been crying alot these past few days and i mean alot and no i know ur super busy and i dont want to put u thoug more ik ur classes are even harder then mine 
 class Slime(Enemy):
     def setup_extra_vars(self):
         self.speed = 5
@@ -33,12 +56,18 @@ class Slime(Enemy):
         self.position += dt
 
 class Skeleton(Enemy):
-    def setup_extra_vars(self):
+    def setup_extra_vars(self): # still need this lol
         self.speed = 0
-    def tick(self, target):
-        dt = dir_to(target, self.position) * self.speed
-        # print(dt)
-        self.position += dt
+
+    #def setup_extra_vars(self):
+    def tick(self, target, ticks):
+
+        if ticks % 50 == 0:
+            print("should shoot")
+            shoot(Bullet, target, self.position + (self.size / 2))
+    
+
+        
     # def shootCD(self, ticker, shoot, Bullet, playerPos):
     #     shoot(Bullet, playerPos, self.position + (self.size / 2))
 
--- main.py
+++ main.py
@@ -1,75 +1,104 @@
 import pygame
 import math
+import random
 from bullet_types import Bullet
-from Enemy import Slime, Skeleton
-#♒︎□︎□︎♌︎◆︎⬧︎ ♑︎●︎□︎□︎♌︎◆︎⬧︎
-#i like apples and bananas
-#me too man, me too
-# nobody cares !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#i care ;-;
-#hiiiiii i love jay<3
-# if you want to test: copy/paste all the code into a local vscode (another window)
-# ctrl a -> ctrl c -> move windows -> ctrl a -> ctrl v
-
-screen = pygame.display.set_mode((640, 360))
-
-player_pos = pygame.Vector2(0, 0)
-player_size = pygame.Vector2(32, 32)
-mouse_pos = pygame.Vector2(0, 0)
-player_speed = 5
-
-running = True
-clock = pygame.time.Clock()
-
-slime = Slime((600, 200), "#00FF00", (32, 32))
-skeleton = Skeleton((610, 310), "#808080", (20, 20))
-
-ticker = 0
-
-# Skeleton = Skeleton((400, 300), (211,211,211), (50, 50))
 
 bullets = []
 
 def shoot(bullet_type, target, position):
     bullet = bullet_type(target, position) # bullet_type should be a class
-    print(bullet)
     global bullets
     bullets.append(bullet)
 
-def get_axis(negative, positive):
-    return pygame.key.get_pressed()[positive] - pygame.key.get_pressed()[negative]
+print(__name__)
 
-while running:
-    ticker += 1
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: running = False
-        if event.type == pygame.MOUSEMOTION: mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
-        if event.type == pygame.MOUSEBUTTONDOWN: shoot(Bullet, mouse_pos, player_pos + (player_size / 2))
-    
-    player_pos.x += get_axis(pygame.K_a, pygame.K_d) * player_speed
-    player_pos.y += get_axis(pygame.K_w, pygame.K_s) * player_speed
+if __name__ == "__main__": # prevents an error
+    from Enemy import Slime, Skeleton
 
-    if ticker > 50:
-        ticker = 0
-        shoot(Bullet, player_pos, skeleton.position + (skeleton.size / 2))
+    screen = pygame.display.set_mode((640, 360))
 
-    #for i in bullets: i.draw(screen); i.tick()
-    
-    screen.fill("#FFFFFF")
+    pygame.init()
 
-    for i in bullets:
-        i.draw(screen)
-        i.tick()
-    
-    slime.tick(player_pos)
-    slime.draw(screen)
+    player_pos = pygame.Vector2(0, 0)
+    player_size = pygame.Vector2(32, 32)
+    mouse_pos = pygame.Vector2(0, 0)
+    player_speed = 5
 
-    skeleton.draw(screen)
+    running = True
+    clock = pygame.time.Clock()
 
-    pygame.draw.rect(screen, (21,21,21), (player_pos, player_size))
+    slime = Slime((600, 200), "#00FF00", (32, 32), 50)
+    skeleton = Skeleton((610, 310), "#808080", (20, 20), 20)
+    ticker = 0
 
-    pygame.display.update()
+    Level = 1
 
-    clock.tick(60)
+    # Skeleton = Skeleton((400, 300), (211,211,211), (50, 50))
 
-pygame.quit()
+    def get_axis(negative, positive):
+        return pygame.key.get_pressed()[positive] - pygame.key.get_pressed()[negative]
+
+    pygame.mixer.music.load("bgm.mp3") # we need doom eternal ost "all they have to fear is you" to survive
+    pygame.mixer.music.play(loops=-1)
+
+    sounds_to_load = [
+        "fire.wav"
+    ]
+
+    enemies = [
+        skeleton
+    ]
+
+    sounds = []
+
+    for i in sounds_to_load:
+        sounds.append(pygame.mixer.Sound(i))
+        print(f"loaded {i}")
+
+    def play_sound(min_id, max_id):
+        sid = random.randint(min_id, max_id)
+        sounds[sid].play()
+
+    while running:
+        ticker += 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: running = False
+            if event.type == pygame.MOUSEMOTION: mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+            if event.type == pygame.MOUSEBUTTONDOWN: shoot(Bullet, mouse_pos, player_pos + (player_size / 2)); play_sound(0, 0)
+        
+        player_pos.x += get_axis(pygame.K_a, pygame.K_d) * player_speed
+        player_pos.y += get_axis(pygame.K_w, pygame.K_s) * player_speed
+
+        #for i in bullets: i.draw(screen); i.tick()
+
+        bfr = enemies.copy()
+
+        for i in enemies:
+            i.tick(player_pos, ticker)
+
+            if i.collide_all(bullets): i.hp -= 10; print(i)
+            if not i.isAlive: bfr.remove(i)
+        
+        enemies = bfr.copy()
+        
+        screen.fill("#FFFFFF")
+
+        for i in bullets:
+            i.draw(screen)
+            i.tick()
+        
+        for i in enemies:
+            i.draw(screen)
+
+        pygame.draw.rect(screen, (21,21,21), (player_pos, player_size))
+        pygame.draw.rect(screen, (225,225,225), (player_pos, (10, 10)))
+
+        pygame.display.update()
+
+        clock.tick(60)
+
+    pygame.quit()
+
+#♒︎□︎□︎♌︎◆︎⬧︎ ♑︎●︎□︎□︎♌︎◆︎⬧︎
+# if you want to test: copy/paste all the code into a local vscode (another window)
+# ctrl a -> ctrl c -> move windows -> ctrl a -> ctrl v
